3cf3cadf531f588e03f7631f11a830424c96c503
NipuniYR <nipuniyr@gmail.com>
2020-07-23 02:05:39 +0530

    Firestore - create collection

---
+++ android/app/src/screens/AddRoomScreen.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import { IconButton, Title } from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
+
+export default function AddRoomScreen({navigation}){
+    const [roomName, setRoomName] = useState('');
+
+    function handleButtonPress() {
+      if (roomName.length > 0) {
+        firestore()
+          .collection('THREADS')
+          .add({
+            name: roomName
+            })
+          .then(() => {
+            navigation.navigate('Home');
+          });
+      }
+    }
+
+    return (
+        <View style={styles.rootContainer}>
+          <View style={styles.closeButtonContainer}>
+            <IconButton
+              icon='close-circle'
+              size={36}
+              color='#7800FF'
+              onPress={() => navigation.goBack()}
+            />
+          </View>
+          <View style={styles.innerContainer}>
+            <Title style={styles.title}>Create a new chat room</Title>
+            <FormInput
+              labelName='Room Name'
+              value={roomName}
+              onChangeText={(text) => setRoomName(text)}
+              clearButtonMode='while-editing'
+            />
+            <FormButton
+              title='Create'
+              modeValue='contained'
+              labelStyle={styles.buttonLabel}
+              onPress={() => handleButtonPress()}
+              disabled={roomName.length === 0}
+            />
+          </View>
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    rootContainer: {
+      flex: 1,
+    },
+    closeButtonContainer: {
+      position: 'absolute',
+      top: 30,
+      right: 0,
+      zIndex: 1,
+    },
+    innerContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 10,
+    },
+    buttonLabel: {
+      fontSize: 22,
+    },
+  });
